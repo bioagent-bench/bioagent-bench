1df80104065d5be95ff440c5e42d3b2b993e0099
dionizijefa <dionizije.fa@hotmail.com>
2025-01-23 13:48:20 +0100

    Edit prefetching bash script

--- virus/scripts/prefetch.sh
+++ virus/scripts/prefetch.sh
@@ -3,5 +3,6 @@ SRA_FILE="sra_ids.txt"
 while read -r SRR_ID; do
   [[ -z "$SRR_ID" ]] && continue
   echo "Prefetching $SRR_ID ..."
-  prefetch "$SRR_ID" -o "../data/$SRR_ID/"
+  mkdir -p "../${SRR_ID}"
+  prefetch "$SRR_ID" -o "../data/${SRR_ID}/"
 done < "$SRA_FILE"
