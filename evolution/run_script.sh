d405590d172cb2b452f6bee37ae7ded4211b0c97
dionizijefa <dionizije.fa@hotmail.com>
2025-02-07 21:30:18 +0100

    Create an env for each tool due to non resolveable envs

--- evolution/run_script.sh
+++ evolution/run_script.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Create individual environments for each tool
+conda create -n fastp_env -c bioconda fastp -y
+conda create -n fastqc_env -c bioconda fastqc -y
+conda create -n multiqc_env -c bioconda multiqc -y
+conda create -n spades_env -c bioconda spades -y
+conda create -n quast_env -c bioconda quast -y
+
 # create the folders and download the data
 mkdir analysis
 cd analysis
@@ -11,6 +18,7 @@ conda env create -f ../environment.yml
 
 # QUALITY CONTROL
 # trim the adapters
+conda activate fastp_env
 fastp --detect_adapter_for_pe --overrepresentation_analysis --correction --cut_right --thread 60 \
  --html trimmed/anc.fastp.html --json trimmed/anc.fastp.json -i data/anc_R1.fastq.gz \
  -I data/anc_R2.fastq.gz -o trimmed/anc_R1.fastq.gz -O trimmed/anc_R2.fastq.gz
@@ -24,13 +32,17 @@ fastp --detect_adapter_for_pe --overrepresentation_analysis --correction --cut_r
  -I data/evol2_R2.fastq.gz -o trimmed/evol2_R1.fastq.gz -O trimmed/evo2_R2.fastq.gz
 
  # run fastqc
+ conda activate fastqc_env
  fastqc -o trimmed-fastqc trimmed/*.fastq.gz
+
 
  # (optional - visualization) visualize the results using multiqc
  # !you need to install multiqc
+ conda activate multiqc_env
  multiqc trimmed-fastqc trimmed
 
  # GENOME ASSEMBLY
+ conda activate spades_env
  mkdir assembly
  spades.py -o assembly/spades-150/ --careful -1 trimmed/anc_R1.fastq.gz -2 trimmed/anc_R2.fastq.gz
  spades.py -o assembly/spades-original/ --careful -1 data/anc_R1.fastq.gz -2 data/anc_R2.fastq.gz
