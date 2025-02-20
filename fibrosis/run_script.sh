9e3b770859a0095204c6351b107f021c75e81252
dionizijefa <dionizije.fa@hotmail.com>
2025-02-20 09:44:34 +0100

    Cystic fibrosis pipeline working

--- fibrosis/run_script.sh
+++ fibrosis/run_script.sh
@@ -10,7 +10,25 @@ mamba activate fibrosis_env
 snpEff download -v GRCh37.75
 snpEff -Xmx12g -v -lof -motif -nextProt GRCh37.75 data/protocols/ex1.vcf > data/ex1.eff.vcf
 
+# this converts the csv metadata to tfam format
 awk -F',' 'NR>1 {print "CEPH_1463", $1, ($3=="0" ? "0" : $3), ($4=="0" ? "0" : $4), $5, $6}' family_tree_metadata.csv > data/pedigree.tfam
 
 SnpSift -Xmx100g caseControl -v -tfam data/pedigree.tfam data/ex1.eff.vcf \
   > data/ex1.eff.cc.vcf
+
+cat data/ex1.eff.cc.vcf \
+| SnpSift filter \
+"(Cases[0] = 3) & (Controls[0] = 0) & ((EFF[*].IMPACT = 'HIGH') | (EFF[*].IMPACT = 'MODERATE'))" \
+> data/ex1.filtered.vcf
+
+wget -O data/clinvar_2025.vcf.gz https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar_20250217.vcf.gz
+gunzip -k data/clinvar_2025.vcf.gz
+
+SnpSift -Xmx100g annotate -v data/clinvar_2025.vcf data/ex1.eff.cc.vcf \
+> data/ex1.eff.cc.clinvar.vcf
+
+cat data/ex1.eff.cc.clinvar.vcf \
+| SnpSift filter \
+    "(GENEINFO[*] =~ 'CFTR') & ((EFF[*].IMPACT = 'HIGH') | (EFF[*].IMPACT = 'MODERATE')) & (Cases[0] = 3) & (Controls[0] = 0)" \
+> output_cf_variant.txt
+
