0bf93e7dd3fd6a6f07d89bca2a280426a0c3ed7a
dionizijefa <dionizije.fa@hotmail.com>
2025-02-24 13:22:11 +0100

    Initial preprocessing steps of deseq up to deseq

--- deseq/run_script.sh
+++ deseq/run_script.sh
@@ -35,4 +35,35 @@ mkdir data/processing/4_fastqc_trimmed
 mkdir data/processing/5_multiqc_trimmed
 
 fastqc data/processing/2_trimming/*fastq -o data/processing/4_fastqc_trimmed -t 32
-multiqc data/processing/2_trimming -o data/processing/4_multiqc_trimmed
+multiqc data/processing/4_fastqc_trimmed -o data/processing/5_multiqc_trimmed
+
+mkdir data/reference
+wget -O data/reference/C_parapsilosis_CDC317_current_chromosomes.fasta.gz http://www.candidagenome.org/download/sequence/C_parapsilosis_CDC317/current/C_parapsilosis_CDC317_current_chromosomes.fasta.gz
+wget -O data/reference/C_parapsilosis_CDC317_current_features.gff http://www.candidagenome.org/download/gff/C_parapsilosis_CDC317/C_parapsilosis_CDC317_current_features.gff
+gunzip -k data/reference/C_parapsilosis_CDC317_current_chromosomes.fasta.gz
+
+mamba install -c bioconda salmon gffread
+mkdir data/processing/6_gff
+gffread -w data/processing/6_gff/transcripts.fasta \
+    -W -F -g data/reference/C_parapsilosis_CDC317_current_chromosomes.fasta \
+    data/reference/C_parapsilosis_CDC317_current_features.gff
+
+mkdir data/processing/7_indexing
+salmon index -t data/processing/6_gff/transcripts.fasta -i data/processing/7_indexing/cpar_index
+
+mkdir data/processing/8_mapping/quants
+
+for file in data/processing/0_fasterqdump/*_1.fastq; do
+    base=$(basename "$file" _1.fastq)
+    
+    salmon quant -i data/processing/7_indexing/cpar_index/ \
+        -l A \
+        -1 data/processing/2_trimming/${base}_1P.fastq \
+        -2 data/processing/2_trimming/${base}_2P.fastq \
+        -p 32 \
+        --validateMappings \
+        -o data/processing/8_mapping/quants/${base}_quant
+done    
+
+mkdir data/processing/9_multiqc_quant
+multiqc data/processing/8_mapping/quants -o data/processing_9_multiqc_quant
