6865017d36499e80004a547e9558f4e553d5b731
dionizijefa <dionizije.fa@hotmail.com>
2025-02-26 14:27:35 +0100

    Completed trimming of the metagenomiccs fastas

--- metagenomics/run_script.sh
+++ metagenomics/run_script.sh
@@ -5,4 +5,28 @@ wget -O data/MGRAST_MetaData_JP.xlsx https://zenodo.org/records/7010950/files/MG
 unzip data/metagenomics.zip -d data/
 
 mamba create -n metagenomics
+mamba activate metagenomics
+
 mamba install -c bioconda fastqc
+
+mkdir -p data/processing/0_fastqc
+fastqc data/seq/*fastq.gz -o data/processing/0_fastqc -t 32
+
+mamba install -c bioconda trimmomatic
+mkdir data/processing/1_trimmed
+
+for file in data/seq/*.fastq.gz; do
+    gunzip -k $file
+done
+
+for file in data/seq/*_R1.fastq; do
+    base=$(basename "$file" _R1.fastq) 
+    trimmomatic PE -threads 32 \
+        data/seq/${base}_R1.fastq \
+        data/seq/${base}_R2.fastq \
+        data/processing/1_trimmed/${base}_1P.fastq \
+        data/processing/1_trimmed/${base}_1U.fastq \
+        data/processing/1_trimmed/${base}_2P.fastq \
+        data/processing/1_trimmed/${base}_2U.fastq \
+        SLIDINGWINDOW:4:20 MINLEN:35 ILLUMINACLIP:data/seq/TruSeq3-PE.fa:2:40:15
+done
